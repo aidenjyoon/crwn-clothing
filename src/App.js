8dd9df2716bc298fb87e95d281039e6100f5235c
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-02-02 19:07:01 -0800

    use Card Component with dummy var

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import Card from "./components/Card/Card.component";
+
+const categories = [
+  {
+    id: 1,
+    title: "Hat",
+  },
+  {
+    id: 2,
+    title: "Jackets",
+  },
+  {
+    id: 3,
+    title: "Sneakers",
+  },
+  {
+    id: 4,
+    title: "Womens",
+  },
+  {
+    id: 5,
+    title: "Mens",
+  },
+];
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="categories-container">
+        {categories.map((category) => {
+          return <Card category={category.title} />;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
