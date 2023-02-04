10b0fa5377ecb7d7c2670642b7706e6a3ab70625
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-02-04 14:48:55 -0800

    navigation logo and style

--- src/routes/navigation/Navigation.component.js
+++ src/routes/navigation/Navigation.component.js
@@ -1,10 +1,17 @@
 import { Outlet, Link } from "react-router-dom";
 
+<<<<<<< Updated upstream
+=======
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./Navigation.styles.scss";
+>>>>>>> Stashed changes
 const NavigationBar = () => {
   return (
     <>
       <div className="navigation">
         <Link className="logo__container" to="/">
+<<<<<<< Updated upstream
           <div className="logo">logo</div>
         </Link>
         <div className="nav_links__container">
@@ -13,6 +20,18 @@ const NavigationBar = () => {
           </Link>
         </div>
         <h1>I'm the navbar</h1>
+=======
+          <CrwnLogo />
+        </Link>
+        <div className="nav_links__container">
+          <Link className="nav_link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav_link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
+>>>>>>> Stashed changes
       </div>
       <Outlet />
     </>
