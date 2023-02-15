463b8f9eefddd4d386c853dd376fd8db8996c727
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-02-14 18:36:59 -0800

    style form inputs

--- src/components/FormInput/FormInput.component.js
+++ src/components/FormInput/FormInput.component.js
@@ -2,9 +2,18 @@ import "./FormInput.styles.scss";
 
 const FormInput = ({ label, ...otherProps }) => {
   return (
-    <div>
-      <label>{label}</label>
-      <input {...otherProps} />
+    <div className="group">
+      <input className="form_input" {...otherProps} />
+
+      {label && (
+        <label
+          className={`${
+            otherProps.value.length ? "shrink" : ""
+          } form_input_label`}
+        >
+          {label}
+        </label>
+      )}
     </div>
   );
 };
