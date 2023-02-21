61e898bf99018c416176ae846102b9b0d533e1dd
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-02-21 14:48:13 -0800

    authentication component style

--- src/routes/authentication/Authentication.component.js
+++ src/routes/authentication/Authentication.component.js
@@ -2,13 +2,17 @@ import { useEffect } from "react";
 import SignUpForm from "../../components/SignUpForm/SignUpForm.component";
 import SignInForm from "../../components/SignInForm/SignInForm.component";
 
+import "./Authentication.styles.scss";
+
 const Authentication = () => {
   return (
     <>
       <h1>Sign In page</h1>
 
-      <SignInForm />
-      <SignUpForm />
+      <div className="auth__container">
+        <SignInForm />
+        <SignUpForm />
+      </div>
     </>
   );
 };
