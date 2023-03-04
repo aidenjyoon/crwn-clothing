f2f88c1e5ceda64d4585859224c5685e09e6075c
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-03-03 20:24:13 -0800

    fixed button layout

--- src/components/CartDropdown/CartDropdown.component.js
+++ src/components/CartDropdown/CartDropdown.component.js
@@ -1,13 +1,13 @@
 import "./CartDropdown.styles.scss";
 import Button from "../Button/Button.component";
+import CartItem from "../CartItem/CartItem.component";
 
 const CartDropdown = () => {
   return (
     <>
       <div className="cart_dropdown__container">
-        <div className="cart_items">
-          <Button>GO TO CHECKOUT</Button>
-        </div>
+        <div className="cart_items"></div>
+        <Button>GO TO CHECKOUT</Button>
       </div>
     </>
   );
