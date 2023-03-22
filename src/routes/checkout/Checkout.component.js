cabec67fdd2c6d804cbe83c456aac70b4d5041d3
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-03-21 19:12:39 -0700

    Update Checkout.component.js

--- src/routes/checkout/Checkout.component.js
+++ src/routes/checkout/Checkout.component.js
@@ -29,19 +29,6 @@ const Checkout = () => {
         {cartItems.map((cItem) => {
           return <CheckoutItem key={cItem.id} cartItem={cItem} />;
         })}
-        {/* {cartItems.map((cItem) => {
-        const { id, name, imageUrl, price, quantity } = cItem;
-        return (
-          <div key={id}>
-            <h2>{name}</h2>
-            <span>{quantity}</span>
-            <br />
-            <span onClick={() => removeItemFromCart(cItem)}>decrement</span>
-            <br />
-            <span onClick={() => addItemToCart(cItem)}>increment</span>
-          </div>
-        );
-      })} */}
 
         <span className="total">Total: {totalPrice}</span>
       </div>
