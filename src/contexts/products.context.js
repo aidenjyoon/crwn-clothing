4593087c4ed65b3d59fe4ff35df0059109d499d8
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-03-21 20:52:55 -0700

    populate firestore with shop data

--- src/contexts/products.context.js
+++ src/contexts/products.context.js
@@ -1,6 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import PRODUCTS from "../shop-data.json";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+import SHOP_DATA from "../shop-data.js";
 
 export const ProductsContext = createContext({
   products: [],
@@ -8,8 +10,12 @@ export const ProductsContext = createContext({
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
   const value = { products, setProducts };
+
+  useEffect(() => {
+    addCollectionAndDocuments("categories", SHOP_DATA);
+  }, []);
 
   return (
     <ProductsContext.Provider value={value}>
