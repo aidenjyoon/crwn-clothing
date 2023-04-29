15f33a7a4daf1752ab478b96f8583df9eca8031a
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-04-28 19:47:53 -0700

    added util function for dispatch
    createAction is created to reduce human error typing {type: ..., payload: ...} inside dispatch()

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,5 +1,7 @@
 import { createContext, useReducer } from "react";
 
+import { createAction } from "../utils/reducer/reducer.utils";
+
 // HELPER FUNCTIONS //
 
 const clearCartItem = (cartItems, cartItemToClear) => {
@@ -113,14 +115,13 @@ export const CartProvider = ({ children }) => {
       0
     );
 
-    dispatch({
-      type: CART_ACTION_TYPES.SET_CART_ITEMS,
-      payload: {
+    dispatch(
+      createAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
         cartItems: newCartItems,
         totalNumberItems: newCartCount,
         totalPrice: newTotalPrice,
-      },
-    });
+      })
+    );
   };
 
   const addItemToCart = (productToAdd) => {
@@ -136,10 +137,7 @@ export const CartProvider = ({ children }) => {
   };
 
   const setIsCartOpen = (cartOpenBool) => {
-    dispatch({
-      type: CART_ACTION_TYPES.SET_IS_CART_OPEN,
-      payload: cartOpenBool,
-    });
+    dispatch(createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, cartOpenBool));
   };
 
   const value = {
--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -1,4 +1,5 @@
 import { createContext, useEffect, useReducer } from "react";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 import {
   onAuthStateChangedListener,
@@ -17,8 +18,6 @@ export const USER_ACTION_TYPES = {
 };
 
 const userReducer = (state, action) => {
-  console.log("dispatched");
-  console.log(action);
   const { type, payload } = action;
 
   switch (type) {
@@ -43,7 +42,7 @@ export const UserProvider = ({ children }) => {
   const { currentUser } = state;
   console.log(currentUser);
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
