c83e92c9f5e2e813e57b4de7830095ab1cf9b05f
aidenjyoon <46219072+aidenjyoon@users.noreply.github.com>
2023-05-02 20:51:46 -0700

    created user.reducer for redux

---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,23 @@
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+// CHANGES NEEDS TO BE MADE WHEN USING REDUX
+// 1) because we don't have hook to call userReducer anymore, need to initiate as the default value. Hence, 'state = INITIAL_STATE'
+export const userReducer = (state = INITIAL_STATE, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+
+    // 2) reducers in redux will receive all action that is dispatched in root-reducer.
+    // as a result need to return default state.
+    default:
+      return state;
+  }
+};
